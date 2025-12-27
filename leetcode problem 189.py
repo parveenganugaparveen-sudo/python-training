d376939c6cb8cbb610877bfb51b4337daf3fe982
parveenganugaparveen-sudo <parveenganugaparveen@gmail.com>
2025-12-27 12:17:36 +0530

    Add files via upload

---
+++ leetcode problem 189.py
@@ -0,0 +1,11 @@
+ k=k%len(nums)
+        '''nums[:]=nums[-k:]+nums[:-k]'''
+        def reve(l,r):
+            while l<r:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+                r-=1
+        reve(0,len(nums)-1)
+        reve(0,k-1)
+        reve(k,len(nums)-1)
+        
