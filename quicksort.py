3eb39b1d71ed8aae3f3ac444f7ecfeb1645d840d
parveenganugaparveen-sudo <parveenganugaparveen@gmail.com>
2025-12-30 16:22:56 +0530

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,21 @@
+def partition(arr,low,high):
+    pivot=arr[high]
+    i=low-1
+    for j in range(low,high):
+        if arr[j]<pivot:
+            i+=1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+def quickSort(arr,low,high):
+    if low<high:
+        k=partition(arr,low,high)
+        quickSort(arr,low,k-1)
+        quickSort(arr,k+1,high)
+arr=list(map(int,input().split()))
+n=len(arr)
+print("orinal array:",arr)
+quickSort(arr,0,n-1)
+print("sorted array:",arr)
+            
+            
